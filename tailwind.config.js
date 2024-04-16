4974a5bfae45fce7e397c8fb855393674fd5aebf
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-16 10:01:05 -0300

    config tailwindcss and adding fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#0063AE',
+        'secondary': '#F2802E',
+      },
+      fontSize: {
+        'xsmall': '0.75rem',
+        'small': '1rem',
+        'medium': '1.25rem',
+        'large': '1.5rem',
+        'xlarge': '2rem',
+        '2xlarge': '3rem',
+      },
+      fontWeight: {
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+      },
+      fontFamily: {
+        'jost': ['Jost', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
