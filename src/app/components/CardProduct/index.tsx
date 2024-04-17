a4c035ecabf8df6e828ffe50aaa5ccfa4dde9c55
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-17 00:26:50 -0300

    Animation LabelCard

--- src/app/components/CardProduct/index.tsx
+++ src/app/components/CardProduct/index.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export function CardProduct({ image, name } : { image: string, name: string}) {
+    const [visible, setVisible] = useState(false);
+
+    const labelStyle = {
+        opacity: visible ? 1 : 0,
+        transition: "opacity 0.3s ease",
+        cursor: "pointer"
+    }
+
     return (
-        <div className="shadow-2xl rounded-xl">
+        <div onMouseEnter={()=> setVisible(true)} onMouseLeave={()=> setVisible(false)} className="shadow-2xl rounded-xl hover:cursor-pointer">
             <img className="w-80 rounded-t-xl" src={image} alt="Imagem do Produto" />
-            <h5 className="text-center p-4 font-semibold">{name}</h5>
+            <div className="flex flex-col items-center py-4">
+                <h5 className="text-center font-semibold">{name}</h5>
+                <label style={labelStyle} className="text-primary font-semibold text-small">{`Ver mais >`}</label>
+            </div>
         </div>
     )
 }
