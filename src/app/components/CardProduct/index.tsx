e61dee73cd4bbe9077defd325f9c72d01708e45d
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-17 00:41:34 -0300

    navigate productDetail

--- src/app/components/CardProduct/index.tsx
+++ src/app/components/CardProduct/index.tsx
@@ -9,13 +9,17 @@ export function CardProduct({ image, name } : { image: string, name: string}) {
         cursor: "pointer"
     }
 
+    function handleNavigate() {
+        window.location.href = "/";
+    }
+
     return (
-        <div onMouseEnter={()=> setVisible(true)} onMouseLeave={()=> setVisible(false)} className="shadow-2xl rounded-xl hover:cursor-pointer">
+        <button type="button" onClick={()=>handleNavigate()} onMouseEnter={()=> setVisible(true)} onMouseLeave={()=> setVisible(false)} className="shadow-2xl rounded-xl hover:cursor-pointer">
             <img className="w-80 rounded-t-xl" src={image} alt="Imagem do Produto" />
             <div className="flex flex-col items-center py-4">
                 <h5 className="text-center font-semibold">{name}</h5>
                 <label style={labelStyle} className="text-primary font-semibold text-small">{`Ver mais >`}</label>
             </div>
-        </div>
+        </button>
     )
 }
