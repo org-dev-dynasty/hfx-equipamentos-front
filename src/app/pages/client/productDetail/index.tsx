71ca7a6b18cac6ebcb16da19df873091b134e3c2
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-17 00:41:48 -0300

    Create ProductDetail

---
+++ src/app/pages/client/productDetail/index.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "../../../components/Footer";
+import { Navbar } from "../../../components/Navbar";
+
+export function ProductDetail() {
+    return (
+        <>
+        <Navbar page="ProductDetail"/>
+        <main>
+            <h1>ASD</h1>
+        </main>
+        <Footer />
+        </>
+    )
+}
