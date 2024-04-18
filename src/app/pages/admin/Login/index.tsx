fe43f03ccacf2f2b8d4fb08cbfccf7e43d7c891e
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-18 16:14:46 -0300

    adding request login

--- src/app/pages/admin/Login/index.tsx
+++ src/app/pages/admin/Login/index.tsx
@@ -1,10 +1,37 @@
+import { useContext, useState } from "react";
 import LoginImage from "../../../assets/loginImage1.png";
 import Logo from "../../../assets/logoLogin.png";
+import { UserContext } from "../../../context/user_context";
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 
 export function SignIn() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const { login } = useContext(UserContext)
+
+    async function doLogin(email: string, password: string) {
+        const response = await login(email, password)
+        console.log(response)
+        localStorage.setItem('token', response?.token ?? "")
+        toast(response?.message, {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    }
+
     return (
         <div className="flex justify-center items-center h-screen">
+            <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
             <img
                 src={LoginImage}
                 alt="Imagem de Login"
@@ -19,6 +46,7 @@ export function SignIn() {
                     <div className="w-full mb-4">
                         <label htmlFor="username" className="block mb-2 font-bold font-jost">Email:</label>
                         <input
+                            onChange={(e) => setEmail(e.target.value)}
                             type="text"
                             id="username"
                             name="username"
@@ -30,6 +58,7 @@ export function SignIn() {
                     <div className="w-full mb-10">
                         <label htmlFor="password" className="block mb-1 font-bold font-jost">Senha:</label>
                         <input
+                            onChange={(e) => setPassword(e.target.value)}
                             type="password"
                             id="password"
                             name="password"
@@ -41,7 +70,8 @@ export function SignIn() {
 
                 </form>
                 <button
-                    type="submit"
+                    onClick={() => doLogin(email, password)}
+                    type="button"
                     className="flex justify-center items-center w-4/5 bg-primary text-white py-2 rounded hover:bg-blue-600 transition duration-500 mt-8 mx-auto"
                 >
                     Entrar
