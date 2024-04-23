a1bb0cacb30eaeddcec153ca2cea9a0610a015b8
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-04-23 15:04:54 -0300

    added link url to test

--- src/app/components/CardProduct/index.tsx
+++ src/app/components/CardProduct/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import EngateRapido from "../../assets/Engate.png";
 import TesouraHidraulica from "../../assets/Tesoura.png";
 import Compactadores from "../../assets/Compactador.png";
-import Rompedores from "../../assets/Rompedor.png";
+// import Rompedores from "../../assets/Rompedor.png";
 import Xorande from "../../assets/apenasXorange.png";
 
 export function CardProduct({ id, image, name } : { id: string, image: string, name: string}) {
@@ -30,7 +30,7 @@ export function CardProduct({ id, image, name } : { id: string, image: string, n
     return (
         <button type="button" onClick={()=>handleNavigate()} onMouseEnter={()=> setVisible(true)} onMouseLeave={()=> setVisible(false)} className={`bg-white duration-500 max-sm:w-[72%] opacity-${opacity} shadow-2xl flex flex-col items-center m-4 rounded-xl hover:cursor-pointer duration-200 hover:scale-105`}>
             <div className="relative">
-                <img className="w-full object-cover h-60 rounded-t-xl" src={name == "Engate Rápido" ? EngateRapido : name == "Tesoura Hidráulica" ? TesouraHidraulica : name == "Compactadores" ? Compactadores : name == "Rompedores" ? Rompedores : image} alt="Imagem do Produto" />
+                <img className="w-full object-cover h-60 rounded-t-xl" src={name == "Engate Rápido" ? EngateRapido : name == "Tesoura Hidráulica" ? TesouraHidraulica : name == "Compactadores" ? Compactadores : name == "Rompedores" ? 'https://hfx-equipamentos-images.s3.sa-east-1.amazonaws.com/HR130%230195d92b-776d-4662-b233-1d14137b1f95.png' : image} alt="Imagem do Produto" />
                 <div className="h-full w-full top-0 absolute opacity-0 duration-200 hover:opacity-100 flex justify-center items-center bg-primary bg-opacity-80 rounded-xl">
                     <img className="w-[100px]" src={Xorande} alt="" />    
                 </div>
